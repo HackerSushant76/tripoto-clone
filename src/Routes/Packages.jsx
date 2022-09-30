ef5f34650907aadbd603691ebb2eab24aaf9fbf3
Sushant <sushantsingh76626@gmail.com>
2022-09-30 23:49:10 +0530

    packages page

--- src/Routes/Packages.jsx
+++ src/Routes/Packages.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import BgImage from '../Components/BgImage'
+import PackageFirstSection from '../Components/PackageFirstSection'
+import Styles from "./Packages.module.css"
 
 function Packages() {
   return (
     <div>
         <BgImage/>
+        <PackageFirstSection />
     </div>
   )
 }
